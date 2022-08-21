1d7e3c3ca804643ab6679264d9e361393f7b1b2a
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-08-22 00:41:47 +0900

    feat(baseball): 컴퓨터의 수와 플레이어의 수를 비교할 수 있는 기능 구현

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ ex) git checkout -b apply-feedback
 # 숫자 야구 게임
 ## 기능 목록
 - [x] 1부터 9까지의 서로 다른 임의의 수 3개를 생성한다. - NumberGenerator#createRandomNumbers()
-- [ ] 컴퓨터의 수(3자리)와 플레이어의 수(3자리)를 비교할 수 있다. - Referee#compare()
+- [x] 컴퓨터의 수(3자리)와 플레이어의 수(3자리)를 비교할 수 있다. - Referee#compare()
   - [x] 몇 개의 숫자가 같은지를 알 수 있다. - Judgement#correctCount()
   - [x] 특정 자리에 특정 숫자가 있는지 알 수 있다. - Judgement#hasPlace()
 - [ ] 같은 수가 같은 자리에 있으면 볼이다.
--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.domain;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 class RefereeTest {
-  
+    @Test
+    void 볼_스트라이크_판별테스트(){
+        Referee referee = new Referee();
+        List<Integer> computer = Arrays.asList(4, 2, 5);
+        String result1 = referee.compare(computer, Arrays.asList(1, 2, 3));
+        String result2 = referee.compare(computer, Arrays.asList(4, 5, 6));
+        String result3 = referee.compare(computer, Arrays.asList(7, 8, 9));
+
+        assertThat("1스트라이크").isEqualTo(result1);
+        assertThat("1볼 1스트라이크").isEqualTo(result2);
+        assertThat("낫싱").isEqualTo(result3);
+    }
 }
