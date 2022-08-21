64ee9729fbe03da0574e2d7bdbb7e864f7cf594f
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-08-22 00:15:46 +0900

    feat(baseball): 특정 자리에 특정 숫자가 있는지 알 수 있는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,4 +1,4 @@
-package baseball.domain;
+package baseball;
 
 public class Application {
 
--- src/test/java/baseball/domain/NumberGeneratorTest.java
+++ src/test/java/baseball/domain/NumberGeneratorTest.java
@@ -1,4 +1,20 @@
+package baseball.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class NumberGeneratorTest {
-  
+
+    @Test
+    void 랜덤넘버생성테스트() {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        List<Integer> randomNumbers = numberGenerator.createRandomNumbers();
+        assertThat(randomNumbers.size()).isEqualTo(3);
+        System.out.println("randomNumbers = " + randomNumbers);
+    }
 }
