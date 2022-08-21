10a12cf32330169bd6ca3547c7b43650b3881135
HOYEONCHOI <chy1995@ajou.ac.kr>
2022-08-22 01:20:36 +0900

    refactor(baseball): 테스트 중복 제거

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -2,7 +2,10 @@ package baseball.domain;
 
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,16 +13,18 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 class RefereeTest {
-    @Test
-    void 볼_스트라이크_판별테스트(){
-        Referee referee = new Referee();
-        List<Integer> computer = Arrays.asList(4, 2, 5);
-        String result1 = referee.compare(computer, Arrays.asList(1, 2, 3));
-        String result2 = referee.compare(computer, Arrays.asList(4, 5, 6));
-        String result3 = referee.compare(computer, Arrays.asList(7, 8, 9));
+    private static final List<Integer> ANSWER = Arrays.asList(4, 2, 5);
+    private Referee referee;
 
-        assertThat("1스트라이크").isEqualTo(result1);
-        assertThat("1볼 1스트라이크").isEqualTo(result2);
-        assertThat("낫싱").isEqualTo(result3);
+    @BeforeEach
+    void setUp() {
+        referee = new Referee();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,2,3,1스트라이크", "4,5,6,1볼 1스트라이크", "7,8,9,낫싱"})
+    public void compare(int number1, int number2, int number3, String expected) {
+        String actual = referee.compare(ANSWER, Arrays.asList(number1, number2, number3));
+        assertThat(actual).isEqualTo(expected);
     }
 }
